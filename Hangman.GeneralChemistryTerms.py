99164f3c9689fb10fcc14000e258750990df2fce
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-02-27 06:03:31 -0500

    Add files via upload

---
+++ Hangman.GeneralChemistryTerms.py
@@ -0,0 +1,6 @@
+genChemWords = ["acid, Arrhenius, base, Bronsted, chemistry, dissociation, density, electron, element"
+          "Freezing, functional group, Gibbs free energy, Gamma emission, Half-reaction, Halogens, indicator, Ionic bond" 
+          "Kinetic energy, kinetic theory, Le Châtelier's principle, Lattice energy, molarity, molality, nitrogen, Neutron"
+          "osmotic pressure, Oxidation, peroxide, Periodic table, Radioactive decay, Raoult's law"
+          "Supersaturated, Salt bridge, Thermodynamics, temperature, Uncertainty principle, Unimolecular reaction"
+          "VSPER theory, valence electron, Wavelength"]
